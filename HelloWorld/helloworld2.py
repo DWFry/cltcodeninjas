278824fc36533d96075257489d85ced6da13958c
0 <0@0.0>
2013-11-07 00:56:31 -0500

    new os independent version of helloworld.py

---
+++ HelloWorld/helloworld2.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# Hello World
+# By Fry - 11/3/2013
+#
+print("Hello World!")
+
+# Scott, any way we can keep the console open after the program runs?
+
+a = input("Press one o' them there non OS-specific keys to get 'er goin")
