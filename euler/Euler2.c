9f3bf2c767fcc90beafc01d9c922bcdf8b021403
DWFry <scottbeam@gmail.com>
2013-11-11 20:24:50 -0500

    update

--- euler/Euler2.c
+++ euler/Euler2.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-		int sum, even;
+		int sum, eventest;
 		int last3[3] = { 1, 2, 4 };
 		
 		while( sum <= 4000000)
@@ -12,9 +12,9 @@ void main()
 			last3[1] = last3[2];
 			last3[2] = last3[0] * last3[1];
 
-			even = last3[2] & 1;
-			
-			if(last3[2] & 1)
+			eventest = last3[2] % 1;
+
+			if(eventest == 0)
 			{
 				sum = sum + last3[2];
 			}
