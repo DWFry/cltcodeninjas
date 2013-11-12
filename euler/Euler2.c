79a73cb10f1f9719e5d1f06a5c051d7eb84e5fc8
DWFry <scottbeam@gmail.com>
2013-11-12 12:54:27 -0500

    update euler2

--- euler/Euler2.c
+++ euler/Euler2.c
@@ -3,8 +3,8 @@
 
 void main()
 {
-	long sum = 2;
-	long a[3] = { 0, 1, 2 };
+	long sum = 0;
+	long a[3] = { 0, 0, 1 };
 
 	while(a[2] < 4000000)
 	{
@@ -12,11 +12,9 @@ void main()
 		{
 			sum = sum + a[2];
  		}
-
 		a[0] = a[1];
 		a[1] = a[2];
 		a[2] = a[0] + a[1];
   	}
-
 	printf ("TOTAL = %d", sum);
 }
