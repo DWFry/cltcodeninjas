da29fe4c3e9a3147425a041d4958b90448c633ab
DWFry <scottbeam@gmail.com>
2013-11-11 19:43:00 -0500

    rearrange

---
+++ euler/Euler2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+		int sum;
+		int last2[2];
+		
+		sum = 0
+		last3 = { 01, 2, 4 };
+
+		while( sum <= 4000000)
+		{
+
+			last3[0] = last1 * last2 };
+			last3[1] = last1 * 2 };
+			last3[2] = last2 * 2 };
+
+		if( )
+
+		}
+		
+		while( mult5 <= 999 )
+		{
+			if( mult5 % 3 != 0 )
+			{
+				total = total + mult5;
+			}
+			mult5 = mult5 + 5;
+		}
+		
+		printf ("TOTAL = %d", total);
+}
---
+++ euler/euler1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+		int total, mult3, mult5;
+		
+       mult3 = 3;
+       mult5 = 5;
+       total = 0;
+       
+		while( mult3 <= 999 )
+		{
+			total = total + mult3;
+			mult3 = mult3 + 3;
+		}
+		
+		while( mult5 <= 999 )
+		{
+			if( mult5 % 3 != 0 )
+			{
+				total = total + mult5;
+			}
+			mult5 = mult5 + 5;
+		}
+		
+		printf ("TOTAL = %d", total);
+}
